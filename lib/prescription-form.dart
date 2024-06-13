492c22a5273b91bddb75afb5e461a3bd39f672cf
Hannah Jacqueline <itshannahjacqueline@gmail.com>
2024-06-13 13:18:06 +0800

    feat[print rx]: initial layout of prescription form

--- lib/prescription-form.dart
+++ lib/prescription-form.dart
@@ -1,30 +1,81 @@
 import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
 import 'dart:io' show Platform;
-
-enum PageFormat { a3, a4, a5, a6 }
 
 class PrescriptionForm {
   final Document _document = Document();
   final String clinicName;
   final String clinicAddress;
   final String doctorName;
+  final List<String> prescriptions;
 
   PrescriptionForm({
     required this.clinicName,
     required this.clinicAddress,
     required this.doctorName,
-    required List<String> prescriptions,
+    required this.prescriptions,
   }) {
-    // TODO: layout the prescription form (header, page number*, and patient's info)
-    _document.addPage(Page(build: (Context context) {
-      return Center(child: Text('Hello World'));
-    }));
+    _generate();
+  }
 
+  void _generate() async {
+    // TODO: layout the prescription form (header, page number*, and patient's info)
     // TODO: adjust form layout if prescription exceeds the page
     // TODO: Add footer for doctor's signature
+    final header = await _header();
+    _document.addPage(
+      Page(
+        margin: const EdgeInsets.all(24),
+        build: (context) => Column(
+          children: [
+            header,
+            Divider(),
+            Text(
+              'Prescribed on June 15, 2023',
+              textAlign: TextAlign.right,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Widget> _header() async {
+    final clinicLogo =
+        (await rootBundle.load('assets/images/rx.png')).buffer.asUint8List();
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+          MemoryImage(clinicLogo),
+          height: 48,
+          width: 48,
+        ),
+        Expanded(
+            child: Column(children: [
+          Text(
+            clinicName,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              height: 1.5,
+            ),
+          ),
+          Text(clinicAddress),
+        ])),
+        Text(
+          'Page 1 of 1',
+          style: TextStyle(fontSize: 10),
+        ),
+      ],
+    );
+  }
+
+  Widget _patientInfo() {
+    return Column(children: [Text('Name:')]);
   }
 
   Future<Uint8List> save() => _document.save();
