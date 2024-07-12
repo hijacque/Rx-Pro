c68cd46d3a41cf7487e8fa12b3a7d93fed399950
HannahJacqueline <itshannahjacqueline@gmail.com>
2024-07-12 13:38:38 +0800

    fix[responsive layout]: add validation for given list of layouts

--- lib/responsive-layout.dart
+++ lib/responsive-layout.dart
@@ -9,15 +9,26 @@ class Layout {
 class ResponsiveLayout extends StatelessWidget {
   final List<Layout> layouts;
 
-  const ResponsiveLayout({
+  ResponsiveLayout({
     super.key,
     required this.layouts,
-  });
+  }) {
+    if (layouts.isEmpty) {
+      throw Exception('ResponsiveLayout Error (1): list of layouts is empty');
+    }
+    // sort layouts in ascending order or breakpoints
+    layouts.sort(
+      (layout1, layout2) => layout1.breakpoint.compareTo(layout2.breakpoint),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      Layout currentLayout = layouts.firstWhere((element) => constraints.maxWidth < element.breakpoint);
+      Layout currentLayout = layouts.firstWhere(
+        (element) => constraints.maxWidth < element.breakpoint,
+        orElse: () => layouts.last,
+      );
       return currentLayout.body;
     });
   }
