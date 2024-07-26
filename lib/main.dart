b43dfdbe8236445e457c4db3b8076c0c60ab7d47
HannahJacqueline <itshannahjacqueline@gmail.com>
2024-07-26 16:23:37 +0800

    style: change primary color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:rxpro_app/pages/doctor-home-page.dart';
+import 'package:rxpro_app/style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: INDIGO),
         useMaterial3: true,
       ),
       home: const DoctorHomePage(),
